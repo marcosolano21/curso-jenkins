9a134a9339a3c9889b0393fe4762478b5537d8f0
Marco Solano <solanohma@gmail.com>
2026-03-11 17:09:39 -0600

    Correccion

--- proyecto-pytest/run_tests.sh
+++ proyecto-pytest/run_tests.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 echo "Activando el entorno virtual"
+
+if[!"-d venv"]; then
+    python3 -m env env
+    
+
+
 source venv/Scripts/activate
 
 echo "Instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "********** Ejecutando pruebas con pytest ***************"
